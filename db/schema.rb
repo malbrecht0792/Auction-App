8971ae1a85ad808aa6b4832e92b2ebe68a28eb08
Marcel <malbrecht0792@gmail.com>
2018-03-24 11:04:05 -0400

    Added user model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324142928) do
+ActiveRecord::Schema.define(version: 20180324150003) do
 
   create_table "auctions", force: :cascade do |t|
     t.datetime "start_date"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180324142928) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
